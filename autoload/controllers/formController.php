38e83949c829ec6c4112f199251c64f102f83970
wanku-x <wanku@bk.ru>
2018-01-03 16:40:11 +0300

    Input validation for buyer-form

--- autoload/controllers/formController.php
+++ autoload/controllers/formController.php
@@ -6,21 +6,40 @@ class formController{
 	{
         $arrayData = $f3->get('POST');
 
+        // Чистка данных
         foreach ($arrayData as &$value) {
             $value = preg_replace('/\s\s+/', ' ', trim(htmlspecialchars(stripslashes($value))));
         }
         unset($value);
 
-        echo "<pre>";
-        print_r($arrayData);
-        echo "</pre>";
+		if (mb_strlen($arrayData[login]) < 1)             { echo "Вы не заполнили поле Логин"; }
+        elseif (mb_strlen($arrayData[login]) > 255)       { echo "Вы ввели больше 255 символов в поле Логин"; }
+        elseif (mb_strlen($arrayData[email]) < 1)         { echo "Вы не заполнили поле Электронная почта"; }
+        elseif (mb_strlen($arrayData[email]) > 255)       { echo "Вы ввели больше 255 символов в поле Электронная почта"; }
+        elseif (mb_strlen($arrayData[password]) < 1)      { echo "Вы не заполнили поле Пароль"; }
+        elseif (mb_strlen($arrayData[password]) > 255)    { echo "Вы ввели больше 255 символов в поле Пароль"; }
+        elseif (mb_strlen($arrayData[name]) < 1)          { echo "Вы не заполнили поле ФИО"; }
+        elseif (mb_strlen($arrayData[name]) > 255)        { echo "Вы ввели больше 255 символов в поле ФИО"; }
+        elseif (mb_strlen($arrayData[phone]) < 1)         { echo "Вы не заполнили поле Телефон"; }
+        elseif (mb_strlen($arrayData[phone]) > 255)       { echo "Вы ввели больше 255 символов в поле Телефон"; }
+        elseif (($arrayData[country]!='ru')&&($arrayData[country]!='es')&&
+                ($arrayData[country]!='gb')&&($arrayData[country]!='us')) { echo "Вы не заполнили поле Страна"; }
+        elseif (mb_strlen($arrayData[mark]) < 1)          { echo "Вы не заполнили поле Маркировка"; }
+        elseif (mb_strlen($arrayData[mark]) > 255)        { echo "Вы ввели больше 255 символов в поле Маркировка"; }
+        elseif (mb_strlen($arrayData[ship]) > 255)        { echo "Вы ввели больше 255 символов в поле Транспортная компания"; }
+        elseif (mb_strlen($arrayData[shipphone]) > 255)   { echo "Вы ввели больше 255 символов в поле Телефон транспортной компании"; }
+        elseif (mb_strlen($arrayData[company]) > 255)     { echo "Вы ввели больше 255 символов в поле Название компании"; }
+        elseif (mb_strlen($arrayData[taxid]) > 255)       { echo "Вы ввели больше 255 символов в поле ИНН"; }
+        elseif (mb_strlen($arrayData[site]) > 255)        { echo "Вы ввели больше 255 символов в поле Сайт"; }
+        elseif (mb_strlen($arrayData[companyinfo]) > 255) { echo "Вы ввели больше 255 символов в поле Информация о компании"; }
+        elseif (mb_strlen($arrayData[bankdetail]) > 255)  { echo "Вы ввели больше 255 символов в поле Банковские данные"; }
+        elseif (($arrayData[person]!='individual')&&($arrayData[person]!='legal'))
+                                                          { echo "Вы не выбрали тип субъекта Гражданского Права"; }
+        elseif (mb_strlen($arrayData[refcode]) > 255)     { echo "Вы ввели больше 255 символов в поле Реферальный код"; }
+        elseif ($arrayData[check]!=1)                     { echo "Вы не приняли Условия использования и заявление о конфиденциальности"; }
+        else                                              { echo "Всё ок!"; }
 
-        /*
-		if ((mb_strlen($secname) < 1)||(mb_strlen($secname) > 255)||
-            (mb_strlen($name) < 1)||(mb_strlen($name) > 255)||
-            (mb_strlen($phone) < 1)||(mb_strlen($phone) > 255)||
-            ($list < 2)||($list > 4))
-        {
+        /*{
             \models\formAction::errorSend($f3, 1, "Вы не заполнили все поля");
         } elseif (($list==2)&&
                  ((mb_strlen($second_name_1) < 1)||(mb_strlen($second_name_1) > 255)||
