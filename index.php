dbef3edbece7eebbc64915fa01681a92fbf95ca1
wanku-x <wanku@bk.ru>
2018-01-03 19:36:44 +0300

    Add DB name, login and password

--- index.php
+++ index.php
@@ -13,15 +13,15 @@ $f3->config('config.ini');
 
 $f3->set('AUTOLOAD', 'autoload/');
 
-/*$db = new DB\SQL('mysql:host=localhost;port=3306;dbname=cm39406_prefect',
-'cm39406_prefect',
-'wl8bh2jkv21n',
+$db = new DB\SQL('mysql:host=localhost;port=3306;dbname=u482956',
+'u482956',
+'SLaveR2nA-.er2',
 array(PDO::MYSQL_ATTR_INIT_COMMAND => 'SET time_zone = \'+03:00\'',
 	  PDO::MYSQL_ATTR_INIT_COMMAND => 'SET CHARACTER SET utf8',
 	  PDO::ATTR_ERRMODE			=> PDO::ERRMODE_EXCEPTION) //для обработки исключений при ошибках в запросах к бд
 );
 
-$f3->set('DB', $db);*/
+$f3->set('DB', $db);
 
 $f3->config('routes.cfg');
 
